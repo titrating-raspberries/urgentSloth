7e7b46a834a209908696c4ebac2edb5dede71aa2
Hack Reactor Students <accounts@hackreactor.com>
2016-02-18 12:37:28 -0800

    small edits to layout, please ignore

--- public/js/services/UserService.js
+++ public/js/services/UserService.js
@@ -3,13 +3,13 @@ angular.module('UserService', []).factory('User', ['$http', function($http) {
   return {
       // call to get all nerds
       get : function() {
-          return $http.get('/api/users');
+          return $http.get('/api/users/');
       },
 
       // these will work when more API routes are defined on the Node side of things
       // call to POST and create a new nerd
       create : function(userData) {
-          return $http.post('/api/users', userData);
+          return $http.post('/api/users/', userData);
       },
 
       // call to DELETE a nerd
