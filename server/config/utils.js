1d842ddb93ba99a5e902ac893c84e3546b1b8373
Leran <leranf#gmail.com>
2016-02-18 16:58:22 -0800

    create allEvents function in eventCtrl

---
+++ server/config/utils.js
@@ -0,0 +1,1 @@
+// add utility functions here
---
+++ server/events/eventController.js
@@ -0,0 +1,78 @@
+var Event = require('./EventModel.js');
+    Q = require('q');
+    util = require('../config/utils.js');
+
+// Promisify a few mongoose methods with the `q` promise library
+var findEvent = Q.nbind(Event.findOne, Event);
+var createEvent = Q.nbind(Event.create, Event);
+var findAllEvents = Q.nbind(Event.find, Event);
+
+module.exports = {
+
+  allEvents: function (req, res, next) {
+  findAllEvents({})
+    .then(function (events) {
+      res.json(links);
+    })
+    .fail(function (error) {
+      next(error);
+    });
+  },
+
+  newEvent: function (req, res, next) {
+    var event = req.body.event;
+    // if (!util.isValidEvent(url)) {
+    //   return next(new Error('Not a valid url'));
+    // }
+
+    findEvent({event: event})
+      .then(function (match) {
+        if (match) {
+          res.send(match);
+        } else {
+          return util.getUrlTitle(url);
+        }
+      })
+      .then(function (title) {
+        if (title) {
+          var newEvent = {
+            url: url,
+            visits: 0,
+            base_url: req.headers.origin,
+            title: title
+          };
+          return createEvent(newEvent);
+        }
+      })
+      .then(function (createdEvent) {
+        if (createdEvent) {
+          res.json(createdEvent);
+        }
+      })
+      .fail(function (error) {
+        next(error);
+      });
+  },
+
+  navToEvent: function (req, res, next) {
+    findEvent({code: req.params.code})
+      .then(function (event) {
+        if (!event) {
+          return next(new Error('Link not added yet'));
+        }
+
+        event.visits++;
+        event.save(function (err, savedEvent) {
+          if (err) {
+            next(err);
+          } else {
+            res.redirect(savedEvent.url);
+          }
+        });
+      })
+      .fail(function (error) {
+        next(error);
+      });
+  }
+
+};
