4a10941d4f0be040ccb97530d7acfc464e0fadee
Leran <leranf#gmail.com>
2016-02-19 19:02:15 -0800

    tests the events html view

--- public/js/controllers/EventsCtrl.js
+++ public/js/controllers/EventsCtrl.js
@@ -3,14 +3,23 @@ angular.module('EventsCtrl', [])
 .controller('EventsController', function($scope, Event) {
 
   $scope.data = {};
-  
-  Event.get()
-    .then(function(events) {
-      $scope.data.events = events;
-    }
-    .catch(function (error) {
+
+
+  // $scope.data.events = [{
+  // 	name: 'super awesome dinner',
+  // 	deadline: new Date(),
+  // 	locations: [{name:'rheas', rating:4, url:'http://sdfsfs'}, {name:'mcdonalds',rating:3, url:'http://sdfasfasfs'}, {name:'carls jr', rating:2, url:'http://sdsdfdssfs'}],
+  // 	dates: [new Date(), new Date(), new Date()],
+  // 	users: [{name:'leran'}, {name:'pavel'}, {name:'taylor'}]
+  // }];
+
+	Event.get()
+		.then(function(events) {
+			$scope.data.events = events;
+		})
+		.catch(function (error) {
       console.error(error);
-    }));
+    });
 
 });
 
