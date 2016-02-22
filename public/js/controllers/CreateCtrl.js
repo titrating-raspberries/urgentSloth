1d61f49734ee38a32604f1441d66d87485f831a7
pavelmp <pavelmp@gmail.com>
2016-02-21 18:09:59 -0800

    Implement CSS for adding/removing friends, Yelp API fully operational

--- public/js/controllers/CreateCtrl.js
+++ public/js/controllers/CreateCtrl.js
@@ -1,5 +1,57 @@
-angular.module('CreateCtrl', []).controller('CreateController', function($scope) {
+angular.module('CreateCtrl', []).controller('CreateController', function($scope, User, Event) {
 
-  $scope.tagline = 'SLOTH';   
+  $scope.friends = []; //List of all users
+  $scope.attendees = {}; //List of friends added to an event
 
+  $scope.lonelyMessage = "...There's nothing quite like sharing a meal with someone you love - yourself...";
+  $scope.showLonelyMessage = true;
+
+  $scope.noLocationsMessage = '“When you make a choice, you change the future.” - Deepak Chopra';
+  $scope.showNoLocationsMessage = true;
+
+  $scope.yelpResults = [];
+  $scope.locations = {};
+
+  var getFriends = function(){
+    //Replace with User.get() when real user database is ready.
+    User.getFake().then(function(friends){
+      $scope.friends = friends;
+    });
+  };
+
+  getFriends();
+
+  $scope.addFriend = function(friend){
+    //Fix when real user database is ready
+    $scope.showLonelyMessage = false;
+    $scope.attendees[friend.user.md5] = friend;
+  };
+
+  $scope.removeFriend = function(friend){
+    delete $scope.attendees[friend.user.md5];
+    $scope.showLonelyMessage = Object.keys($scope.attendees).length === 0 ? true : false;
+  };
+
+  //Fires up Yelp search for restaurants based on 'Add location' form on create.html
+  $scope.submit = function() {
+    if ($scope.term && $scope.location) {
+      Event.searchYelp($scope.term, $scope.location).then(function(results){
+        $scope.yelpResults = results.data.businesses;
+        console.log($scope.yelpResults);
+      })
+    }
+  };
+
+  $scope.addLocation = function(restaurant){
+    //Create a unique for the locations object
+    var uniqueKey = restaurant.location.coordinate.latitude + '-' + restaurant.location.coordinate.longitude;
+    $scope.locations[uniqueKey] = restaurant; 
+    $scope.showNoLocationsMessage = false;
+  };
+
+  $scope.removeLocation = function(restaurant){
+    var uniqueKey = restaurant.location.coordinate.longitude + '-' + restaurant.location.coordinate.longitude;
+    delete $scope.locations[uniqueKey];
+    $scope.showNoLocationsMessage = Object.keys($scope.locations).length === 0 ? true : false;
+  }
 });
--- public/js/services/EventService.js
+++ public/js/services/EventService.js
@@ -15,7 +15,7 @@ angular.module('EventService', [])
         return $http.delete('/api/events/' + id);
     },
 
-    searchYelp: function(term, location, country){
+    searchYelp: function(term, location){
       function randomString(length, chars) {
         var result = '';
         for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
@@ -23,33 +23,31 @@ angular.module('EventService', [])
       };
       
       var yelpUrl = 'https://api.yelp.com/v2/search';
-      var httpMethod = 'JSONP';
+      var httpMethod = 'GET';
 
       //Yelp access token:
-      var consumerKey = 'jENexVIGC2-ho2MPKDCvISyRfPU';
-      var consumerSecret = 'jENexVIGC2-ho2MPKDCvISyRfPU';
+      var consumerKey = 'gsjk0o7MGSveWE1fl3XDJQ';
       var token = 'uosUcU25he8AlgqM8paqwzj-j4mpkIhl';
+      var consumerSecret = 'jENexVIGC2-ho2MPKDCvISyRfPU';
       var tokenSecret = 'myAMTAkatscCizvq2z3cSffp0uc';
 
       //Required parameters for oauth signature and Yelp API request:
-      var parameters = {
-        oauth_consumer_key: consumerKey,
-        oauth_token: token,
-        oauth_signature_method: 'HMAC-SHA1', 
-        oauth_timestamp: new Date().getTime(),
-        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-      };
+      var parameters = {callback: 'angular.callbacks._0',
+                        location: location,
+                        oauth_consumer_key: consumerKey,
+                        oauth_token: token,
+                        oauth_signature_method: "HMAC-SHA1",
+                        oauth_timestamp: new Date().getTime(),
+                        oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+                        term: term,
+                        limit: 10
+                       };
 
       //Generate oauth_signature using 3rd party script (included on index.html):
-      var oauth_signature = oauthSignature.generate(httpMethod, yelpUrl, parameters, consumerSecret, tokenSecret);
+      var oauth_signature = oauthSignature.generate(httpMethod, yelpUrl, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
       parameters['oauth_signature'] = oauth_signature;
 
-      //Generate full request URL:
-      var requestUrl = yelpUrl + '?term=' + term + '&location=' + location + '&cc=' + (country || 'US') + '&limit=' + 15;
-
-      return $http.jsonp({url: requestUrl, param: parameters}).then(function(res){
-        return res.data;
-      })
+      return $http.jsonp(yelpUrl, {params: parameters})
     }
   };       
 }]);
