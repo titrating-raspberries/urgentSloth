c3bfefc5f623d6622736c4a67b3a959989ab4b7a
tchamberlain <taylorannchamberlain@gmail.com>
2016-02-22 15:13:46 -0800

    Testing user picture in nav

--- public/js/controllers/IndexCtrl.js
+++ public/js/controllers/IndexCtrl.js
@@ -2,5 +2,9 @@ angular.module('IndexCtrl', ['ngCookies'])
 .controller('IndexController',function($location,$cookies,$scope) {
   //ensure logout button is not shown on nav bar if user is not logged in
   $scope.loggedIn = ($cookies.get('name')!== undefined);
+  $scope.userPic = $cookies.get('picture');
+  $scope.userName = $cookies.get('name');
+
+
 });
 
