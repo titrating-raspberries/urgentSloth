b03ba751c880ef175f6220fa222e02eb916f6486
tchamberlain <taylorannchamberlain@gmail.com>
2016-02-22 20:33:38 -0800

    Delete auth

--- server/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '1655047711391983', // your App ID
-        'clientSecret'  : '6da4819c4f7124defe1035c55c6682bf', // your App Secret
-        'callbackURL'   : 'http://local.host:3000/auth/facebook/callback'
-    },
-}
