9f64a32f33f1a3ad1e6e1025fb8012caf2b1117d
tchamberlain <taylorannchamberlain@gmail.com>
2016-02-22 19:12:01 -0800

    Add friends prof pic to friends array in db

--- server/users/userController.js
+++ server/users/userController.js
@@ -6,41 +6,51 @@ var User = require('./userModel.js');
 var findUser = Q.nbind(User.findOne, User);
 var createUser = Q.nbind(User.create, User);
 
+//addFriendsPicture will grab a user's friends prof pic links to store in the db
+var addFriendsPicture = function(friendArr){
+  for(var i = 0; i < friendArr.length; i++){
+    var friend = friendArr[i];
+     findUser({fbId: friend.id})
+        .then(function (match) {
+          if(match !== null){
+            friend.picture = match.picture;
+          } else{
+            console.log('userController: Error retrieving friend', match);
+          }
+        });
+  }
+  return friendArr;
+}
+
 module.exports = {
   createOrFindOne: function (profile) {
     var fbId = profile.id;
     var name = profile.displayName;
     var picture = profile.photos[0].value;
-    var friends = profile._json.friends.data;
+    var friends = addFriendsPicture(profile._json.friends.data);
 
       findUser({fbId: fbId})
         .then(function (match) {
-          if (match) {
-            console.log('USER MATCH');
-          } else {
-            var noMatch = true;
-            return noMatch;
-          }
-        })
-        .then(function (noMatch) { // ask about this
-          if (noMatch){
-            console.log('NO MATCH');
+          //if there's no match, we want to create a new user 
+          if (match === null) {
             var newUser = {
               name: name,
               fbId: fbId,
               picture: picture,
               friends: friends
             };
-            return createUser(newUser);
-          }
-        })
-        .then(function (createdUser) {
-          if (createdUser) {
-            console.log('CREATED USER');
+            createUser(newUser);
+          } else {// if user already exists, update user's friends in the database
+            match.friends = friends;
+            match.save(function (err) {
+                if (err){
+                  return handleError(err);
+                } 
+              });
           }
         })
         .fail(function (error) {
-          console.log('CREATE USER Error',error);
+          console.log('createOrFind user Error',error);
           next(error);
         });
     }
