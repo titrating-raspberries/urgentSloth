8d48279b0a656d15677984b44afe03df29316f01
pavelmp <pavelmp@gmail.com>
2016-02-24 17:57:53 -0800

    Continue CSS formating on Events page

--- public/js/controllers/EventsCtrl.js
+++ public/js/controllers/EventsCtrl.js
@@ -15,5 +15,15 @@ angular.module('EventsCtrl', [])
   $scope.logData = function (event) {
     //return Event.update(event);
   };
+})
+.directive('toggleClass', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function() {
+                element.toggleClass(attrs.toggleClass);
+            });
+        }
+    };
 });
 
