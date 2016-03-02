8fe29ba9b419ac8ee8afd28e5045a7948c01cf1f
Chris Bassano <christo4b@gmail.com>
2016-03-02 11:12:32 -0800

    fixed syntax issues from merge conflicts

--- server/events/eventModel.js
+++ server/events/eventModel.js
@@ -10,6 +10,7 @@ var eventSchema = new mongoose.Schema({
     usersWhoSubmitted: [], // array of fb ids of those users who've submitted votes on the date and loc options
     decision: {},// object with decided time and location, undefined until after deadline
     description: String,
+    host: {}, //object that will come from $cookies when event is created
 });
 
 module.exports = mongoose.model('Event', eventSchema);
