c44df12bd24a33386d60c5b6c8d804732b5fb2fe
Boya Jiang <jiang.boya@yahoo.com>
2016-03-03 17:50:12 -0800

    Add event dates and time to calendar and add a legend

--- public/js/controllers/CalendarCtrl.js
+++ public/js/controllers/CalendarCtrl.js
@@ -1,14 +1,39 @@
 angular.module('CalendarCtrl', [])
-.controller('CalendarController',function($scope, $window) {
+.controller('CalendarController',function($scope, $window, $cookies, Event, User) {
 
   $scope.test ="hello world";
-  $scope.library=$window.fullCalendar;
-  console.log($window);
-  // $(document).ready(function() {
-  //     // page is now ready, initialize the calendar...
-  //     $('#calendar').fullCalendar({
+  $scope.calendarEvents = [];
 
-  //         // put your options and callbacks here
-  //     });
-  // });
+  var userFbId = $cookies.get('fbId');
+
+  Event.getUserEvents($cookies.get('fbId'))
+  .then(function(events) {
+    var eventList = events;
+    eventList.forEach(function (event) {
+      var color = '#005ce6'; // color for Needs your vote
+      if (event.decision){
+        color='#2d7b52'; //color for decided
+      } else if (event.usersWhoSubmitted.indexOf(userFbId) !== -1) {
+        color = '#86592d'; //color for Pending
+      }
+      // console.log(event);
+      $scope.calendarEvents.push({
+        title: event.name,
+        start: event.dates[0].date.slice(0, 10),
+        allDay: false,
+        color: color
+      });
+    });
+    $(document).ready(function() {
+      // page is now ready, initialize the calendar...
+      // setTimeout(function (){
+        console.log($scope.calendarEvents);
+        $('#calendar').fullCalendar({
+          events: $scope.calendarEvents,
+            // put your options and callbacks here
+          // eventColor: '#2d7b52'
+        })
+      // }, 500);
+  });
+  });
 });
