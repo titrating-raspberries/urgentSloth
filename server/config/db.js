48dc05f7b57676d28fd0225e5b888e940e20d9e3
Boya Jiang <jiang.boya@yahoo.com>
2016-03-03 23:15:26 -0800

    change back to new database server

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
   // url : 'mongodb://localhost/urgentSloth'
-  url: 'mongodb://urgentsloth:sloth4hackreactor@ds019648.mlab.com:19648/heroku_dfjz8fcq'
-  // url: 'mongodb://admin:titratingraspberries@ds019698.mlab.com:19698/heroku_8kb4zd2s'
+  // url: 'mongodb://urgentsloth:sloth4hackreactor@ds019648.mlab.com:19648/heroku_dfjz8fcq'
+  url: 'mongodb://admin:titratingraspberries@ds019698.mlab.com:19698/heroku_8kb4zd2s'
 };
 
 
