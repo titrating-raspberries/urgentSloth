218882eb0a4c7836c6f9345cf5487b0ec30c8242
Boya Jiang <jiang.boya@yahoo.com>
2016-03-03 21:16:43 -0800

    Add modal on click with styled information

--- public/js/controllers/CalendarCtrl.js
+++ public/js/controllers/CalendarCtrl.js
@@ -19,19 +19,47 @@ angular.module('CalendarCtrl', [])
       } else if (event.usersWhoSubmitted.indexOf(userFbId) !== -1) {
         color = '#86592d'; //color for Pending
       }
-      // console.log(event);
+      var currentDescription = event.description || '';
+      var currentHost = event.host ? event.host.name : '';
+      console.log(event);
       $scope.calendarEvents.push({
         title: event.name,
         start: event.dates[0].date.slice(0, 10),
         allDay: false,
-        color: color
+        color: color,
+        eventData: {
+          name: event.name,
+          description: currentDescription,
+          host: currentHost,
+        }
       });
     });
     $(document).ready(function() {
       console.log($scope.calendarEvents);
       $('#calendar').fullCalendar({
         events: $scope.calendarEvents,
-          // put your options and callbacks here
+        eventLimit: true, // for all non-agenda views
+        views: {
+          agenda: {
+            eventLimit: 6 // adjust to 6 only for agendaWeek/agendaDay
+          }
+        },
+        header: {
+        left: '',
+        center: 'prev title next',
+        right: ''
+        },
+        eventClick:  function(event, jsEvent, view) {
+          $("#eventInfo").html('<div><span style="font-weight: bold">Event</span>: ' + event.eventData.name +
+            '</div><div><span style="font-weight: bold">Description</span>: ' + event.eventData.description
+            + '</div><div><span style="font-weight: bold">Host</span>: ' + event.eventData.host + '</div>'
+          );
+          $('#eventContent').dialog({
+            modal: true,
+            maxHeight:500,
+          }).prev(".ui-dialog-titlebar").css("background","#2d7b52");
+          $('.ui-dialog :button').blur();
+        },
       })
     });
   });
