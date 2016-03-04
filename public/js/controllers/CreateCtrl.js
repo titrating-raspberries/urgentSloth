b4c6349825d1b3bad8ee6cbf07d5eb481bb444eb
Alex Bignell <alex@bignell.me>
2016-03-04 11:43:06 -0800

    Add HTML5 geolocation button to location-form

--- public/js/controllers/CreateCtrl.js
+++ public/js/controllers/CreateCtrl.js
@@ -44,9 +44,23 @@ angular.module('CreateCtrl', []).controller('CreateController', function($scope,
     $scope.showLonelyMessage = Object.keys($scope.attendees).length === 0 ? true : false;
   };
 
-  Location.ip().then(function(ipLocation) {
-    $scope.location = ipLocation;
-  }, console.error);
+  Location.ip()
+    .then(function(ipLocation) {
+      $scope.location = ipLocation;
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+
+  $scope.getUserLocation = function() {
+    Location.browser()
+      .then(function(browserLocation) {
+        $scope.location = browserLocation;
+      })
+      .catch(function(err) {
+        $scope.locationError = err;
+      });
+  };
 
   //Fires up Yelp search for restaurants based on 'Add location' form on create.html
   $scope.submit = function() {
