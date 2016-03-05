f533667f7b069044385338e24c1aed8576617f16
Boya Jiang <jiang.boya@yahoo.com>
2016-03-05 12:34:46 -0800

    Fully functioning cron job and correct filtering for notifying users about upcoming events

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -14,71 +14,62 @@ var createEvent = Q.nbind(Event.create, Event);
 var updateEvent = Q.nbind(Event.update, Event);
 var findAllEvents = Q.nbind(Event.find, Event);
 var getAllUsers = Q.nbind(User.find, User);
-
 var findUser = Q.nbind(User.findOne, User);
 var getAllUsers = Q.nbind(User.find, User);
 
 
 var checkEventAndNotify = function() {
-  console.log('CHECKEVENTANDNOTIFY');
   findAllEvents({ notified: false })
   .then(function (events) {
     var EventsToNotify = [];
     events.forEach( function (event) {
       if (event.decision){
-        EventsToNotify.push(event);
+        var currentDate = new Date();
+        var timeDiff = Math.abs(event.decision.date.getTime() - currentDate.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        if(diffDays<4){
+          EventsToNotify.push(event);
+        };
       };
     });
 
     EventsToNotify.forEach(function(event){
-      updateEvent({name: event.name}, {notified: 'true'}, { multi: false }, function(err, success){console.log("RESULT OF NOTIFIED CHANGE ON EVENT ISSSSSSSS", err,'or', success)});
+      updateEvent({name: event.name}, {notified: 'true'}, { multi: false }, function(err, success){console.log(err,'or', success)});
       getAllUsers({ 'fbId':  { $in: event.users}})
       .then(function (userList) {
         var emailList= [];
         userList.forEach(function (user) { emailList.push(user.email.value); });
         emailListStr = emailList.join(', ');
         emailList.forEach( function(email) {
-          console.log('send email to', email);
+          var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+          var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
           var transporter = nodemailer.createTransport('smtps://webdevtestserver@gmail.com:titrating-raspberries@smtp.gmail.com');
-          var mailOptions = {
-              from: '"Your Events" <events@whereandwhen.com>', // sender address
-              to: email, // list of receivers
-              subject: 'You have an event coming up!', // Subject line
-              text: 'Hello world', // plaintext body
-              html: '<b>Hello world </b>' // html body
-          };
 
           transporter.sendMail({
-          from: 'walkingonglass@gmail.com',
+          from: 'yourfriends@whenandwhere.com',
             to: email,
             subject: 'You have an upcoming event!',
-            text: 'Please visit When and Where to check for you upcoming',
+            text: 'To our esteemed partier' + ',\n\nYour scheduled event, ' + event.name + ', is scheduled at ' + event.decision.location.name + ' on ' +
+              days[(event.decision.date.getDay())] + ', ' + months[(event.decision.date.getMonth())] + ' ' + event.decision.date.getDate() +
+              ', ' + event.decision.date.getFullYear() +'. \nPlease visit When&Where for more details!\n\nSincerely,\nThe When&Where Team', //Mon Mar 07 2016
           }, function(error, info){
               if (error){
                 console.log('error is ', error);
               } else if (info){
-                console.log('info is ', info);
-              } else {
-                console.log('nothing is happening');
+                console.log('email return object is ', info);
               }
           });
         });
       });
-
     });
-
   });
 }
 
-checkEventAndNotify();
-
-// var notifications = new CronJob('*/ * * * *', function() {
-
-//   },
-//   checkEventAndNotify,
-//   true,
-//   timeZone: 'America/Los_Angeles'
-// );
+var notifications = new CronJob('*/5 * * * *', //Cron Event for checking whether to send event notifications
+  checkEventAndNotify,
+  true,
+  'America/Los_Angeles'
+);
 
 
 var pickWinner = function(choices, category) {
@@ -107,7 +98,6 @@ var decideUsersEvents = function(fbId) {
                 if((event.deadline < new Date() && event.decision === undefined) ||
                     event.usersWhoSubmitted.length === event.users.length) {
                   var decision = makeEventDecision(event);
-                  console.log('decision made!!');
                   Event.update({_id: event._id}, {decision: decision}, function (err, savedEvent) {
                       if (err) {
                         console.error(err);
@@ -253,7 +243,6 @@ module.exports = {
             next(err);
           } else {
             if (event.usersWhoSubmitted.length === event.users.length) {
-              console.log('about to decide event');
               decideUsersEvents(userFbId);
             }
             res.send(savedEvent);
